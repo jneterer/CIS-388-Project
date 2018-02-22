abeb246259b92c8bc6a587a91a55cdfc3235eebb
Jacob Neterer <jacobneterer@gmail.com>
2018-02-22 10:40:31 -0500

    Added nodemon, connect to DB, save new user, etc

--- public/server/models/user.js
+++ public/server/models/user.js
@@ -1,19 +1,37 @@
-const mongoose = require('mongoose');
-const bycript = require('bycript');
-
-var UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    passwordConf: {
-        type: String,
-        required: true
-    }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Defining User Schema
+var UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1
+  },
+  password: {
+    type: String,
+    min: [6, 'Minimum of 6 characters'],
+    require: true
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  books: {
+    type: Array,
+    required: false
+  }
 });
+
+var User = mongoose.model('User', UserSchema)
+module.exports = {User};
--- public/server/server.js
+++ public/server/server.js
@@ -1,58 +1,36 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoClient = require('mongodb').MongoClient;
-const f = require('util').format;
+require('./config/config');
 
-var uri = 'mongodb://bookshelf-shard-00-00-fieye.mongodb.net:27017,bookshelf-shard-00-01-fieye.mongodb.net:27017,bookshelf-shard-00-02-fieye.mongodb.net:27017/bookshelf?ssl=true&replicaSet=Bookshelf-shard-0&authSource=admin';
-// Found answer on how to do this correctly here https://www.questarter.com/q/cannot-connect-to-mongodb-in-azure-27_48029723.html
-MongoClient.connect(uri, {user: 'jnetererAdmin', password: '806260#Ae'}, function(err, client) {
-    if(!err) {
-        console.log('We are connected');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  var user = new User({
+    first_name: 'Jacob',
+    last_name: 'Neterer',
+    email: 'jacobneterer@gmail.com',
+    password: '1234567890',
+    phone: '7178301858'
+  });
+  user.save((err) => {
+    if (err) {
+      console.log(err);
     }
     else {
-        console.log(err);
+      console.log('Saved!');
     }
+  })
 });
-//
-// var db = mongoose.connection;
-//
-// // Use sessions for tracking logins
-// app.use(session({
-//     secret: 'whatever',
-//     resave: true,
-//     saveUninitialized: false,
-//     store: new MongoClient({
-//         mongooseConnection: db
-//     })
-// }));
-//
-// // Parse incoming requests
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false}));
-//
-// // Serve static files from template
-// app.use(express.static(__dirname + '../../public'));
-//
-// // Includes routes
-// var routes = require('./router.js');
-// app.use('./', routes);
-//
-// // Catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     var err = new Error('File Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-//
-// // Error handler
-// // Define as the last app.use callback
-// app.use(function (err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.send(err.message);
-// });
+
+app.get('/login', (req, res) => {
+  res.sendFile('/login.html', {root: __dirname + '../../login'});
+});
 
 // Listen on port 3000
 app.listen(3000, function() {
