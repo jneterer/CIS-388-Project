57090fe837d20d83bf178e7fc8ea33b34cabb93a
Jacob Neterer <jacobneterer@gmail.com>
2018-02-26 02:14:11 -0500

    Check for login working.

--- public/server/app.js
+++ public/server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
+const ensure = require('connect-ensure-login');
+ensure.defaultRedirectUrl = '/login';
+ensure.defaultReturnUrl = '/home';
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -17,53 +21,67 @@ app.use(express.static(__dirname + '../../../public'));
 app.use(express.static(__dirname + '../../../public/home'));
 app.use(express.static(__dirname + '../../../Images'));
 
+app.use(session({
+  secret: 'aye1',
+  saveUnitialized: true,
+  resave: true
+}));
+
 //GET login.html
+app.use(passport.initialize());
+app.use(passport.session());
+
+const auth = require('./authenticate/passport');
+
+
+// passport.serializeUser(function(user, done) {
+//   done(null, user._id);
+// });
+//
+// passport.deserializeUser(function(_id, done) {
+//   console.log('heyyo');
+//   User.findById(_id, function(err, user) {
+//     done(err, user);
+//   });
+// });
+//
+// passport.use(new LocalStrategy({
+//     usernameField: 'email',
+//     passwordField: 'password'
+//   }, function(email, password, done) {
+//     User.findOne({email}, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Email or password is incorrect.' });
+//       }
+//       bcrypt.compare(password, user.password, (err, res) => {
+//         if (res) {
+//           return done(null, user);
+//         } else {
+//           return done(null, false, { message: 'Email or password is incorrect.' });
+//         }
+//       });
+//     });
+//   })
+// );
+
 app.get('/login', (req, res) => {
   res.sendFile('/login.html', {root: __dirname + '../../login'});
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(_id, done) {
-  console.log('heyyo');
-  User.findById(_id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  }, function(email, password, done) {
-    User.findOne({email}, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Email or password is incorrect.' });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          return done(null, user);
-        } else {
-          return done(null, false, { message: 'Email or password is incorrect.' });
-        }
-      });
-    });
-  })
-);
-
-app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req, res) => {
+app.post('/login', passport.authenticate('local', {successReturnToOrRedirect: '/', failureRedirect: '/login'}), (req, res) => {
   res.redirect('/home');
 });
 
-app.get('/home', (req, res) => {
-  console.log('oops');
+app.get('/home', ensure.ensureLoggedIn('/login'), (req, res) => {
   res.sendFile('/home.html', {root: __dirname + '../../home'});
 });
+
+
+app.get('/my_library', ensure.ensureLoggedIn('/login'), (req, res) => {
+  res.sendFile('/home.html', {root: __dirname + '../../my_library'});
+});
+
 
 // Listen on port 3000
 app.listen(3000, function() {
--- public/server/authenticate/passport.js
+++ public/server/authenticate/passport.js
@@ -1,35 +1,37 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-
-app.use(passport.initialize());
-app.use(passport.session());
+var {mongoose} = require('../db/mongoose');
+var {User} = require('../models/user');
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
-  }, function(email, email, done) {
-    User.findOne({ email: email }, function (err, user) {
+  }, function(email, password, done) {
+    User.findOne({email}, function (err, user) {
       if (err) { return done(err); }
       if (!user) {
-        return done(null, false, { message: 'Incorrect username or password.' });
+        return done(null, false, { message: 'Email or password is incorrect.' });
       }
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
           return done(null, user);
         } else {
-          return done(null, false, { message: 'Incorrect password.' });
+          return done(null, false, { message: 'Email or password is incorrect.' });
         }
+      });
     });
-  });
-});
+  })
+);
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  console.log(user._id);
+  done(null, user._id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
+passport.deserializeUser(function(_id, done) {
+  console.log('heyyo');
+  User.findById(_id, function(err, user) {
     done(err, user);
   });
 });
