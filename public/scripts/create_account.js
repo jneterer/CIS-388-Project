9ef5735a3394609dd6f2c5f3291b404190d69550
Jacob Neterer <jacobneterer@gmail.com>
2018-04-10 22:08:13 -0400

    Changes

--- public/scripts/create_account.js
+++ public/scripts/create_account.js
@@ -1,13 +1,5 @@
 function validateForm() {
-  var first_name = document.forms['create_account_form']['first_name'].value;
-  var last_name = document.forms['create_account_form']['last_name'].value;
-  var email = document.forms['create_account_form']['email'].value;
-  var password = document.forms['create_account_form']['password'].value;
-  var confirmPassword = document.forms['create_account_form']['confirmPassword'].value;
-  if (first_name === '' || last_name === '' || email === '' || password === '' || confirmPassword === '') {
-    alert('All required fields must be filled out!');
-    return false;
-  } else if (password !== confirmPassword) {
+  if (password !== confirmPassword) {
     alert('Passwords do not match!');
     return false;
   }
